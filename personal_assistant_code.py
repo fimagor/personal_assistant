ec7d71839a6686f7a90cfd4c8c4d1224e2d46615
fimagor <131290530+fimagor@users.noreply.github.com>
2023-05-10 19:39:22 +0300

    Update personal_assistant_code.py
    Доработка исключений, исправление ошибки импорта библиотек

--- personal_assistant_code.py
+++ personal_assistant_code.py
@@ -1,85 +1,90 @@
 #импорт библиотек
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
+from tkinter import filedialog
 from tkinter import font
 from datetime import datetime
-from tkcalendar import *
-from tkinter import filedialog
+from tkcalendar import Calendar
+
 import pickle
 
+# Константы
+
+BG_WIND = ('#00FFFF')
+BG_BUT = '#F0FFFF'
 
 #закрытие приложения
 def on_close():
   if messagebox.askokcancel("Выйти", "Хотите выйти?"):
-    tk.destroy()
+    wind1.destroy()
 
 #Создание окна
-tk = Tk()
-tk.protocol("WM_DELETE_WINDOW", on_close)
-tk.title('Личный помощник')
-tk.wm_attributes("-topmost", 1)
-tk.geometry("500x500")
-tk.configure(bg='#00FFFF')
+wind1 = tk.Tk()
+wind1.protocol("WM_DELETE_WINDOW", on_close)
+wind1.title('Личный помощник')
+wind1.wm_attributes("-topmost", 1)
+wind1.geometry("500x500")
+wind1.configure(bg=BG_WIND)
 
 #шрифты
 my_font = font.Font(family="Arial", size=30, weight="bold")
 
-frame = Frame(tk)
+frame = tk.Frame(wind1)
 frame.pack(pady=10)
 
 
 # Создание списка дел
-list = Listbox(frame, font=my_font, width=25, height=5, bg="SystemButtonFace", bd=0, fg="#0000CC",
+list = tk.Listbox(frame, font=my_font, width=25, height=5, bg="SystemButtonFace", bd=0, fg="#0000CC",
                highlightthickness=0, selectbackground="#6699FF", activestyle="none")
-list.pack(side=LEFT, fill=BOTH)
+list.pack(side=tk.LEFT, fill=tk.BOTH)
 
 # Прокрутка
-scrollbar = Scrollbar(frame)
-scrollbar.pack(side=RIGHT, fill=BOTH)
+scrollbar = tk.Scrollbar(frame)
+scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
 
-list.config(yscrollcommand=scrollbar.set, bg='#F0FFFF')
+list.config(yscrollcommand=scrollbar.set, bg=BG_BUT)
 scrollbar.config(command=list.yview)
 
 # Поле ввода заметки
-entry = Entry(tk, font=my_font, bg='#F0FFFF')
+entry = tk.Entry(wind1, font=my_font, bg=BG_BUT)
 entry.pack(pady=20)
 
-button_frame = Frame(tk, bg='#00FFFF')
+button_frame = tk.Frame(wind1, bg=BG_WIND)
 button_frame.pack(pady=20)
 
 # Функция удаления записи
 def delete_item():
     if len(list.curselection())==0:
-        messagebox.showerror('ошибка', 'Список дел пуст')
+        messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
     else:
-        list.delete(ANCHOR)
+        list.delete(tk.ANCHOR)
 
 
 # Функция добавления записи
 def add_item():
-    list.insert(END, entry.get())
-    entry.delete(0, END)
+    list.insert(tk.END, entry.get())
+    entry.delete(0, tk.END)
 
 # Функция вычеркивания записи
 def cross_of_item():
     if len(list.curselection()) == 0:
-        messagebox.showerror('ошибка', 'Список дел пуст')
+        messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
     else:
         list.itemconfig(list.curselection(), fg='#CCCCFF')
-        list.select_clear(0, END)
+        list.select_clear(0, tk.END)
 
 # Функция отмены зачеркивания записи
 def uncross_item():
     if len(list.curselection()) == 0:
-        messagebox.showerror('ошибка', 'Список дел пуст')
+        messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
     else:
         list.itemconfig(list.curselection(), fg='#0000CC')
-        list.select_clear(0, END)
+        list.select_clear(0, tk.END)
 
 
 # Функция удаления зачеркнутого
 def delete_crossed():
-    if len(list.curselection()) == 0:
+    if list.size() == 0:
         messagebox.showerror('ошибка', 'Список дел пуст')
     else:
         cnt=0
@@ -92,13 +97,13 @@ def delete_crossed():
 
 
 # Создание кнопок
-delete_button = Button(button_frame, text="Удалить", command=delete_item, bg='#F0FFFF')
-add_button = Button(button_frame, text="Добавить", command=add_item, bg='#F0FFFF')
+delete_button = tk.Button(button_frame, text="Удалить", command=delete_item, bg=BG_BUT)
+add_button = tk.Button(button_frame, text="Добавить", command=add_item, bg=BG_BUT)
 #add_button.grid(row=0, column=0)
 #add_button.bind('<Button-1>', add_item)
-cross_off_button = Button(button_frame, text="Выполнено", command=cross_of_item, bg='#F0FFFF')
-uncross_button = Button(button_frame, text="Невыполнено", command=uncross_item, bg='#F0FFFF')
-delete_crossed_button = Button(button_frame, text="Удалить выполненное", command=delete_crossed, bg='#F0FFFF')
+cross_off_button = tk.Button(button_frame, text="Выполнено", command=cross_of_item, bg=BG_BUT)
+uncross_button = tk.Button(button_frame, text="Невыполнено", command=uncross_item, bg=BG_BUT)
+delete_crossed_button = tk.Button(button_frame, text="Удалить выполненное", command=delete_crossed, bg=BG_BUT)
 
 
 # Расположение кнопок
@@ -116,85 +121,84 @@ actual_datetime = datetime.now()
 # Функция создания календаря
 def create_calendar():
     # Создаем новое окно
-    wind2 = Toplevel(tk)
+    wind2 = tk.Toplevel(wind1)
     wind2.title('Календарь')
     wind2.wm_attributes("-topmost", 1)
     wind2.geometry('400x400')
-    wind2.configure(bg='#00FFFF')
+    wind2.configure(bg=BG_WIND)
     cal = Calendar(wind2, selectmode='day', year=actual_datetime.year, month=actual_datetime.month,
                    day=actual_datetime.day)
     cal.pack()
 
     # Выбор даты
     def take_date():
-        global date
         date = cal.get_date()
-        label.config(text=date,  bg='#00FFFF')
+        label.config(text=date,  bg=BG_WIND)
 
 
-    label = Label(wind2, text='', bg='#00FFFF')
+    label = tk.Label(wind2, text='', bg=BG_WIND)
     label.pack(pady=20)
 
     def note():
-        wind3 = Toplevel(tk)
+        wind3 = tk.Toplevel(wind1)
         wind3.title("")
         wind3.wm_attributes("-topmost", 1)
         wind3.geometry("500x500")
-        wind3.configure(bg='#00FFFF')
+        wind3.configure(bg=BG_WIND)
 
-        frame = Frame(wind3)
+        frame = tk.Frame(wind3)
         frame.pack(pady=10)
 
         # Создание списка дел
-        list = Listbox(frame, font=my_font, width=25, height=5, bg="SystemButtonFace", bd=0, fg="#0000CC",
+        list = tk.Listbox(frame, font=my_font, width=25, height=5, bg="SystemButtonFace", bd=0, fg="#0000CC",
                        highlightthickness=0, selectbackground="#6699FF", activestyle="none")
-        list.pack(side=LEFT, fill=BOTH)
+        list.pack(side=tk.LEFT, fill=tk.BOTH)
 
         # Прокрутка
-        scrollbar = Scrollbar(frame)
-        scrollbar.pack(side=RIGHT, fill=BOTH)
+        scrollbar = tk.Scrollbar(frame)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
 
-        list.config(yscrollcommand=scrollbar.set, bg='#F0FFFF')
+        list.config(yscrollcommand=scrollbar.set, bg=BG_BUT)
         scrollbar.config(command=list.yview)
 
         # Поле ввода заметки
-        entry = Entry(wind3, font=my_font, bg='#F0FFFF')
+        entry = tk.Entry(wind3, font=my_font, bg=BG_BUT)
         entry.pack(pady=20)
 
-        button_frame = Frame(wind3, bg='#00FFFF')
+        button_frame = tk.Frame(wind3, bg=BG_WIND)
         button_frame.pack(pady=20)
 
         # Функция удаления записи
         def delete_item():
             if len(list.curselection()) == 0:
-                messagebox.showerror('ошибка', 'Список дел пуст')
+                messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
             else:
-                list.delete(ANCHOR)
+                list.delete(tk.ANCHOR)
 
         # Функция добавления записи
         def add_item():
-            list.insert(END, entry.get())
-            entry.delete(0, END)
+            list.insert(tk.END, entry.get())
+            entry.delete(0, tk.END)
 
         # Функция вычеркивания записи
         def cross_of_item():
             if len(list.curselection()) == 0:
-                messagebox.showerror('ошибка', 'Список дел пуст')
+                messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
             else:
                 list.itemconfig(list.curselection(), fg='#CCCCFF')
-                list.select_clear(0, END)
+                list.select_clear(0, tk.END)
 
         # Функция отмены зачеркивания записи
         def uncross_item():
             if len(list.curselection()) == 0:
-                messagebox.showerror('ошибка', 'Список дел пуст')
+                messagebox.showerror('ошибка', 'Вы не выбрали заметку ')
             else:
                 list.itemconfig(list.curselection(), fg='#0000CC')
-                list.select_clear(0, END)
+                list.select_clear(0, tk.END)
 
         # Функция удаления зачеркнутого
         def delete_crossed():
-            if len(list.curselection()) == 0:
+            if list.size() == 0:
                 messagebox.showerror('ошибка', 'Список дел пуст')
             else:
                 cnt = 0
@@ -203,13 +207,12 @@ def create_calendar():
                         list.delete(list.index(cnt))
                     cnt += 1
 
-
         # Создание кнопок
-        delete_button = Button(button_frame, text="Удалить", command=delete_item, bg='#F0FFFF')
-        add_button = Button(button_frame, text="Добавить", command=add_item, bg='#F0FFFF')
-        cross_off_button = Button(button_frame, text="Выполнено", command=cross_of_item, bg='#F0FFFF')
-        uncross_button = Button(button_frame, text="Невыполнено", command=uncross_item, bg='#F0FFFF')
-        delete_crossed_button = Button(button_frame, text="Удалить выполненное", command=delete_crossed, bg='#F0FFFF')
+        delete_button = tk.Button(button_frame, text="Удалить", command=delete_item, bg=BG_BUT)
+        add_button = tk.Button(button_frame, text="Добавить", command=add_item, bg=BG_BUT)
+        cross_off_button = tk.Button(button_frame, text="Выполнено", command=cross_of_item, bg=BG_BUT)
+        uncross_button = tk.Button(button_frame, text="Невыполнено", command=uncross_item, bg=BG_BUT)
+        delete_crossed_button = tk.Button(button_frame, text="Удалить выполненное", command=delete_crossed, bg=BG_BUT)
 
         # Расположение кнопок
         add_button.grid(row=0, column=0)
@@ -223,17 +226,17 @@ def create_calendar():
         take_date()
 
     # Кнопка выбора даты
-    take_date_button = Button(wind2, text='Выбрать дату', command=call_func, bg='#F0FFFF')
+    take_date_button = tk.Button(wind2, text='Выбрать дату', command=call_func, bg=BG_BUT)
     take_date_button.pack(pady=20)
 
 
 # Кнопка создания вкладки календаря
-button_cal = Button(tk, text="Календарь",command=create_calendar, bg='#F0FFFF')
+button_cal = tk.Button(wind1, text="Календарь",command=create_calendar, bg=BG_BUT)
 button_cal.pack()
 
 # Создание меню
-my_menu = Menu(tk)
-tk.config(menu=my_menu)
+my_menu = tk.Menu(wind1)
+wind1.config(menu=my_menu)
 
 # Функция сохранения файла
 def save_file():
@@ -245,7 +248,7 @@ def save_file():
             pass
         else:
             file_name = f"{file_name}.dat"
-    all_list = list.get(0, END)
+    all_list = list.get(0, tk.END)
 
     output_file = open(file_name, 'wb')
 
@@ -257,24 +260,24 @@ def open_file():
                                              filetypes=(("Dat Files", "*.dat"),
                                              ("All Files", "*.*")))
     if file_name:
-        list.delete(0, END)
+        list.delete(0, tk.END)
 
         input_file = open(file_name, 'rb')
 
     my_list = pickle.load(input_file)
 
     for i in my_list:
-        list.insert(END, i)
+        list.insert(tk.END, i)
 
 # Функция удаления файла
 def delete_file():
-    list.delete(0, END)
+    list.delete(0, tk.END)
 
-file_menu = Menu(my_menu, tearoff=False)
+file_menu = tk.Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label='Файл', menu=file_menu)
 file_menu.add_command(label='Сохранить файл', command=save_file)
 file_menu.add_command(label='Открыть файл', command=open_file)
 file_menu.add_command(label='Удалить расписание', command=delete_file)
 
 
-tk.mainloop()
+wind1.mainloop()
